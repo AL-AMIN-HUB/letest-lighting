4ea0339dc24fc1e70368ebe752244c142711cf62
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2021-10-15 18:40:19 +0600

    toggle button clicked in js

---
+++ app.js
@@ -0,0 +1,6 @@
+let btn = document.getElementById("btn");
+let light = document.getElementById("light");
+const toggleBtn = () => {
+  btn.classList.toggle("active");
+  light.classList.toggle("on");
+};
